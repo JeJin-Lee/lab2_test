5734188d4a50fa5c15dc614277f085d656b7b375
jejin <lwjssy72@naver.com>
2022-09-05 19:35:15 +0900

    add count.cc

---
+++ count.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int i=1;
+    while(i<11){
+        cout << "i" << i << endl;
+        i++;
+    }
+    cout << "Start!!" << endl;
+    cout << "[2017313283] [JeJin Lee]" << endl;
+    return 0;
+}
+
